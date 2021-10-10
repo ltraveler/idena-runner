706e7ce5783445ee4d7bf7b7e5c410496f9f7d61
ltraveler <ravil.sadreev@gmail.com>
2021-10-10 03:02:04 +0100

    + possibility to add your own frequency to check for available idena-go updates

--- idena_install.sh
+++ idena_install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #
 #ASCII color assign
+RED="\033[0;31m"
 LRED="\033[1;31m"
-YELLOW="\033[1;33m"
+YELLOW="\033[0;33m"
+LYELLOW="\033[1;33m"
+GREEN="\033[0;32m"
 LGREEN="\033[1;32m"
+BLUE="\033[0;34m"
 LBLUE="\033[1;34m"
+PURPLE="\033[0;35m"
+LPURPLE="\033[1;35m"
+CYAN="\033[0;36m"
+LCYAN="\033[1;36m"
 NC="\033[0m" # No Color
 #Checking if the idena service exists.
 if [ -f "/etc/systemd/system/idena.service" ]
@@ -117,7 +125,9 @@ systemctl enable idena.service
 #Checking for idena updates ones a day
 cp idena_insp.sh /home/$username/idena-go/
 chown $username:$username /home/$username/idena-go/idena_insp.sh
-crontab -l | grep -q 'idena_insp'  && echo 'entry exists' || (crontab -l 2>/dev/null; echo "0 1 * * * /home/$username/idena-go/idena_insp.sh") | crontab -
+read -p "Please insert the frequency in cron schedule expressions format when the script will be checking for updates. Empty prompt will set the value to once a day at 1AM: " idupdate
+if [[ -z $idupdate ]]; then idupdate=$(echo "0 1 * * *") ; echo "Setting as $idupdate"; fi
+crontab -l | grep -q 'idena_insp'  && echo 'entry exists' || (crontab -l 2>/dev/null; echo "$idupdate /home/$username/idena-go/idena_insp.sh") | crontab -
 # ufw configuration
 SSHPORT=${SSH_CLIENT##* }
 ufw disable
