2ec480ed80270693c2b2f36e60479fecf1117821
ltraveler <ravil.sadreev@gmail.com>
2021-10-17 08:18:18 +0100

    some modifications related to multiple distance idena node daemon

--- idena_insp.sh
+++ idena_insp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 latver=$( curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep -Po '"tag_name":.*?[^\\]",' | sed 's/"tag_name": "v//g' |  sed 's/",//g' ) 
-username=$( cat /etc/systemd/system/idena.service | grep 'User=' | sed 's/User\=//g' )
+username=$( cat /etc/systemd/system/idena_$USER.service | grep 'User=' | sed 's/User\=//g' )
 version=$( cat /home/$username/idena-go/version )
 echo $latver
 echo $username
@@ -10,12 +10,12 @@ cver=$( echo $version | sed 's/[^0-9]*//g' )
 if [ $lver -gt $cver ]
 then
 echo "New version is available"
-service idena stop
+service idena_$username stop
 wget https://github.com/idena-network/idena-go/releases/download/v$latver/idena-node-linux-$latver
 chmod +x idena-node-linux-$latver
 chown $username:$username idena-node-linux-$latver
 mv idena-node-linux-$latver /home/$username/idena-go/idena-node
-service idena start
+service idena_$username start
 echo $latver > /home/$username/idena-go/version
 echo "New version has been succefully  installed"
 fi
