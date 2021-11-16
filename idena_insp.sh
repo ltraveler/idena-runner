2550baf0ddf62a8b0cf578387cad01dd3d73c026
ltraveler <ravil.sadreev@gmail.com>
2021-11-16 20:08:40 +0000

    idena instance update script bug fix

--- idena_insp.sh
+++ idena_insp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 latver=$( curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep -Po '"tag_name":.*?[^\\]",' | sed 's/"tag_name": "v//g' |  sed 's/",//g' ) 
-username=$( cat /etc/systemd/system/idena_$USER.service | grep 'User=' | sed 's/User\=//g' )
+username="$(whoami)"
 version=$( cat /home/$username/idena-go/version )
 echo $latver
 echo $username
 echo $version
 lver=$( echo $latver | sed 's/[^0-9]*//g' )
 cver=$( echo $version | sed 's/[^0-9]*//g' )
-if [ $lver -gt $cver ]
+if [[ "$lver" -gt "$cver" ]]
 then
 echo "New version is available"
 sudo service idena_$username stop
