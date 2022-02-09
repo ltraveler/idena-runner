5f3493ebb5a5557cc0074605268f9a2b15fb068f
Simargl <5625337+ltraveler@users.noreply.github.com>
2022-02-09 01:21:05 -0500

    Update idena_insp.sh

--- idena_insp.sh
+++ idena_insp.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#ASCII color assign
+RED="\033[0;31m"
+LRED="\033[1;31m"
+YELLOW="\033[0;33m"
+LYELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+LGREEN="\033[1;32m"
+BLUE="\033[0;34m"
+LBLUE="\033[1;34m"
+PURPLE="\033[0;35m"
+LPURPLE="\033[1;35m"
+CYAN="\033[0;36m"
+LCYAN="\033[1;36m"
+NC="\033[0m"
 latver=$( curl -s https://api.github.com/repos/idena-network/idena-go/releases/latest | grep -Po '"tag_name":.*?[^\\]",' | sed 's/"tag_name": "v//g' |  sed 's/",//g' ) 
 username="$(whoami)"
 version=$( cat /home/$username/idena-go/version )
@@ -17,5 +31,5 @@ chown $username:$username idena-node-linux-$latver
 mv idena-node-linux-$latver /home/$username/idena-go/idena-node
 sudo service idena_$username start
 echo $latver > /home/$username/idena-go/version
-echo -e "${LRED}IDENA NODE HAS BEEN SUCCESSFULLY UPDATED"
+echo -e "${LRED}IDENA NODE HAS BEEN SUCCESSFULLY UPDATED${NC}"
 fi
